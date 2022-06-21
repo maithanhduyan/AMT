44e2724ef4307f6061f1d71b8d0190879a4c4664
Mai Thành Duy An <tiachop0102@gmail.com>
2022-06-22 02:29:52 +0900

    Client-Server Design.

---
+++ web/src/App.js
@@ -0,0 +1,24 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ProductList from "./components/ProductList";
+import AddProduct from "./components/AddProduct";
+import EditProduct from "./components/EditProduct";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="container">
+        <div className="columns">
+          <div className="column is-half is-offset-one-quarter">
+            <Routes>
+              <Route path="/" element={<ProductList />} />
+              <Route path="add" element={<AddProduct />} />
+              <Route path="edit/:id" element={<EditProduct />} />
+            </Routes>
+          </div>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
